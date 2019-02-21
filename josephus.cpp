e7031721b58ff3828524d0b66c492303de842e28
limjaein <dlawodls96@naver.com>
2019-02-21 20:13:29 +0900

    Update josephus.cpp

--- josephus.cpp
+++ josephus.cpp
@@ -8,24 +8,24 @@ int circle[1000];
 
 void suicide()
 {
-	int sum = 0;
-	int i = 0;
-	int gap = K - 1; // 0 번째 초기화
+	int sum = 0; // 자살한 사람 수
+	int i = 0; // index
+	int gap = K - 1; // 0 번째 초기화를 위해 K - 1로 초기화
 
-	while (sum != (N - 2))
+	while (sum != (N - 2)) // 2명만 살아남을 때 까지 진행
 	{
-		if (circle[i] != 0)
-			gap++; // 간격 ++
+		if (circle[i] != 0) // 살아있는 사람이면 간격에 포함
+			gap++;
 
-		if (gap == K)
+		if (gap == K) // K명째 일 때
 		{
-			circle[i] = 0;
-			sum++;
+			circle[i] = 0; // 자살
+			sum++; // 자살한 사람 수 포함
 			gap = 0; // 간격 초기화
 		}
 
-		i += 1;
-		i %= N;
+		i += 1; // 다음 index
+		i %= N; // 전체 인원을 넘을 경우 N의 나머지 값 
 	}
 }
 
@@ -44,7 +44,7 @@ int main()
 
 		for (int i = 0; i < N; i++)
 		{
-			if (circle[i] == -1)
+			if (circle[i] == -1) // 자살하지 않은 사람의 번호 출력
 				cout << i + 1 << '\t';
 		}
 
